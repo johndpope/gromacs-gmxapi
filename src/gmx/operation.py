26a3029657b91c7a27e6413fadf59319a7783986
M. Eric Irrgang <ericirrgang@gmail.com>
2019-01-02 15:52:18 +0300

    Lay out framework for testing Operation protocols.
    Create gmx.operation module. Describe basic tests and begin iteratively
    building functionality in gmx.test.operationtest module. Create
    test_operation.py test module.

--- src/gmx/operation.py
+++ src/gmx/operation.py
@@ -50,3 +50,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
+
+
+def make_operation():
+    """Wrap a class to produce an object that acts as a gmxapi Operation."""
+    return None
---
+++ src/gmx/test/test_operation.py
@@ -0,0 +1,94 @@
+"""Test the gmx.operation module and Operation protocols."""
+
+import unittest
+
+import gmx
+
+# Import the sample operation module from the directory containing these tests.
+import operationtest
+
+# Test the wrapper as documented.
+
+# class MSMAnalyzer:
+#     """
+#     Builds msm from gmxapi output trajectory
+#     """
+#     def __init__(self, topfile, trajectory, P, N):
+#         # Build markov model with PyEmma
+#         feat = coor.featurizer(topfile)
+#         X = coor.load(trajectory, feat)
+#         Y = coor.tica(X, dim=2).get_output()
+#         k_means = coor.cluster_kmeans(Y, k=N)
+#         centroids = get_centroids(k_means)
+#
+#         M = msm.estimate_markov_model(kmeans.dtrajs, 100)
+#
+#         # Q = n-1 transition matrix, P = n transition matrix
+#         Q = P
+#         self.P = M.get_transition_matrix()  # figure this out
+#         self._is_converged = relative_entropy(self.P, Q) < tol
+#
+#     def is_converged(self):
+#         return self._is_converged
+#
+#     def transition_matrix(self):
+#         return self.P
+#
+# msm_analyzer = gmx.operation.make_operation(MSMAnalyzer,
+#                                             input=['topfile', 'trajectory', 'P', 'N'],
+#                                             output=['is_converged', 'transition_matrix']
+#                                             )
+
+class GraphSetupTestCase(unittest.TestCase):
+    """Test operation nodes being added to the work graph."""
+    @classmethod
+    def setUpClass(cls):
+        """Run before any tests in this class."""
+
+    def setUp(self):
+        """Run before each test.
+        """
+        pass
+
+    def test_implicit_graph(self):
+        """Test that the expected operations and outputs are defined in the default graph.
+
+        Use the scripting interface and the default graphContext to configure a
+        work graph with two operations.
+        """
+        self.operation1 = operationtest.dummy_operation1(param1='', param2='')
+        self.operation2 = operationtest.dummy_operation2(
+            input={'input1': '',
+                   'input2': self.operation1.output.data1})
+
+    def test_explicit_graph(self):
+        """Test that the helper functions also correctly handle explicit context."""
+        pass
+
+    def tearDown(self):
+        """Run after each test."""
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        """Run after these tests and before the next set of tests.
+
+        Clean up global state.
+        """
+
+class GraphLaunchTestCase(unittest.TestCase):
+    """Test the translation of a graph to a runnable session."""
+    def test_launch(self):
+        pass
+
+class ImmediateExecutionTestCase(unittest.TestCase):
+    """Test use of the operation context to execute an operation immediately.
+
+    Execution is not deferred and results are not proxied.
+    """
+    def test_immediate_execution(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()

ee87d6badc36738a0c72884a880b1f0c43788cd4
M. Eric Irrgang <ericirrgang@gmail.com>
2019-01-18 11:21:18 +0300

    Initial commit of infrastructure supporting fork.
    Set up a development branch (not targeted for merging with master, that
    can serve as the basis for a forked repository in which experimental
    features can be developed.
    Based on
    https://github.com/kassonlab/gromacs-gmxapi/commit/bd48b0e8e55428c35ae4aace09c3dbf8c62109af

---
+++ ci_scripts/pygmx_devel.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ev
+
+BRANCH=devel
+
+pushd $HOME
+ rm -rf gmxapi
+ wget https://github.com/kassonlab/gmxapi/archive/${BRANCH}.zip
+ unzip $BRANCH
+ mv gmxapi-$BRANCH gmxapi
+ pushd gmxapi
+  rm -rf build
+  mkdir -p build
+  pushd build
+   cmake .. -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC -DPYTHON_EXECUTABLE=$PYTHON
+   make -j2 install
+   make -j2 docs
+  popd
+ popd
+ mpiexec -n 2 $PYTHON -m mpi4py -m pytest --log-cli-level=DEBUG --pyargs gmx -s --verbose
+ ccache -s
+popd

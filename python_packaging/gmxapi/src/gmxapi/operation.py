7266fb586cf2c8012df7de6f014636f50f355761
M. Eric Irrgang <ericirrgang@gmail.com>
2019-01-24 20:59:06 +0300

    Framework for make_operation.
    Utility to be used to allow easy export of Python classes as compliant
    Operation factories. Generates a factory function that produces a valid
    Operation instance. Generates the Operation subclass to be used by the
    factory. Generates and validates the required Operation interface, which
    includes a callable `run` property and an `output` property with
    prescribed behaviors.
    
    To do:
    * flesh out the Operation behaviors
    * map input, output, and callable expressed in utility function call
      to the wrapped class

--- python_packaging/gmxapi/src/gmxapi/operation.py
+++ python_packaging/gmxapi/src/gmxapi/operation.py
@@ -22,11 +22,65 @@
     assert op2.output.count.extract() == 2
 """
 
-class Operation(object):
-    def output(self):
-        pass
-    def run(self):
-        pass
+from gmxapi.exceptions import ApiError
+
+class Runner(object):
+    """Descriptor class for runner behavior."""
+    def __init__(self, implementation):
+        self.name = None
+        self.internal_name = None
+
+    def __get__(self, instance, instance_type):
+        if instance is None: return self
+        return getattr(instance, self.internal_name, lambda : True)
+
+    def __set__(self, instance, value):
+        setattr(instance, self.internal_name, value)
+
+class Result(object):
+    """Descriptor class for Result behavior."""
+    pass
+
+class OutputMeta(type):
+    """Output proxy metatype to annotate and enforce interface."""
+    def __new__(meta, name, bases, class_dict):
+        for key, value in class_dict.items():
+            if isinstance(value, Result):
+                value.name = key
+                value.internal_name = '_' + key
+        cls = type.__new__(meta, name, bases, class_dict)
+        return cls
+
+class Output(object):
+    """Descriptor class for output proxy behavior."""
+    def __init__(self):
+        self.name = None
+        self.internal_name = None
+
+    def __get__(self, instance, instance_type):
+        if instance is None: return self
+        return getattr(instance, self.internal_name, '')
+
+    def __set__(self, instance, value):
+        setattr(instance, self.internal_name, value)
+
+class ValidateOperation(type):
+    """Confirm that an Operation class implements the gmxapi Operation protocol."""
+    def __new__(meta, name, bases, class_dict):
+        # Don't validate the abstract Operation class
+        if bases != (object,):
+            if class_dict['run'] is None:
+                raise ApiError()
+            else:
+                class_dict['run'].name = 'run'
+                class_dict['run'].internal_name = '_' + 'run'
+            if class_dict['output'] is None:
+                raise ApiError()
+        return type.__new__(meta, name, bases, class_dict)
+
+class Operation(object, metaclass=ValidateOperation):
+    run = None
+    output = None
 
 def make_operation(implementation, input=None, output=None):
     """Utility to create a gmxapi-compliant Operation in an importable module.
@@ -42,8 +96,16 @@ def make_operation(implementation, input=None, output=None):
         A callable that produces objects supporting the gmxapi operation protocol.
 
     """
+    operation_output = output
+    if operation_output is None:
+        operation_output = object()
+
+    class NewOperation(Operation):
+        run = Runner(implementation)
+        output = operation_output
+
     def factory(*args, **kwargs):
-        operation = Operation()
+        operation = NewOperation()
         return operation
 
     return factory

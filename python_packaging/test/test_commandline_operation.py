21c033649c5ee858185ebf6dd5f8c414d0d60453
M. Eric Irrgang <ericirrgang@gmail.com>
2019-01-30 16:11:29 +0300

    Begin importing commandline_operation from gmxpy 0.0.8 dev branch.

--- python_packaging/test/test_commandline_operation.py
+++ python_packaging/test/test_commandline_operation.py
@@ -38,3 +38,50 @@
 gmx.commandline_operation() provides additional logic over gmx.make_operation
 to conveniently wrap command line tools.
 """
+
+class CommandLineOperationTestCase(unittest.TestCase):
+    """Test creation and execution of command line wrapper."""
+    def test_true(self):
+        operation = gmx.commandline_operation(executable='true')
+        assert not 'stdout' in operation.output
+        assert not 'stderr' in operation.output
+        assert not 'returncode' in operation.output
+        # assert not hasattr(operation.output, 'stdout')
+        # assert not hasattr(operation.output, 'stderr')
+        # assert not hasattr(operation.output, 'returncode')
+        success = operation()
+        assert success
+        assert not 'stdout' in operation.output
+        assert not 'stderr' in operation.output
+        # assert not hasattr(operation.output, 'stdout')
+        # assert not hasattr(operation.output, 'stderr')
+        # assert operation.output.returncode == 0
+        assert operation.output['returncode'] == 0
+
+    def test_false(self):
+        operation = gmx.commandline_operation(executable='false')
+        assert not 'stdout' in operation.output
+        assert not 'stderr' in operation.output
+        assert not 'returncode' in operation.output
+        # assert not hasattr(operation.output, 'stdout')
+        # assert not hasattr(operation.output, 'stderr')
+        # assert not hasattr(operation.output, 'returncode')
+        success = operation()
+        assert not success
+        assert not 'stdout' in operation.output
+        assert not 'stderr' in operation.output
+        # assert not hasattr(operation.output, 'stdout')
+        # assert not hasattr(operation.output, 'stderr')
+        # assert operation.output.returncode == 0
+        assert operation.output['returncode'] == 1
+
+    def test_echo(self):
+        operation = gmx.commandline_operation(executable='echo', arguments=['hi there'])
+        success = operation()
+        assert success
+        assert not 'stdout' in operation.output
+        assert not 'stderr' in operation.output
+        # assert not hasattr(operation.output, 'stdout')
+        # assert not hasattr(operation.output, 'stderr')
+        # assert operation.output.returncode == 0
+        assert operation.output['returncode'] == 0
